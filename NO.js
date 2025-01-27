357ca1d911888fbe8b87827394dff55c9d6a5b50
Jacob <t00niyf5@duck.com>
2025-01-27 15:55:21 +0000

    Add debug info (tends to go crazy on some sites)
    Adding debug code.
    TODO: Add a var to turn off debugging

--- NO.js
+++ NO.js
@@ -10,42 +10,64 @@ realTimeout = setTimeout
 realInterval = setInterval
 
 Number.prototype.valueOf = function () {
-    return realPow(Math.random(), 2) * 2 * this.RealValueOf();
+    console.debug("valueOf called with "+ this.RealValueOf());
+    let tret = realPow(Math.random(), 2) * 2 * this.RealValueOf();
+    console.debug("Returning "+tret);
+    return tret;
 };
 
 Boolean.prototype.valueOf = function () {
-    return Math.random() > 0.5;
+    let tret = Math.random() > 0.5;
+    console.debug("Boolean valueOf called. Returning "+tret);
+    return tret;
 }
 
 Math.pow = function (x, y) {
-    return realPow(x.valueOf(), y.valueOf()).valueOf();
+    console.debug("pow called with "+x+", "+y);
+    let tret = realPow(x.valueOf(), y.valueOf()).valueOf();
+    console.debug("Returning "+tret)
+    return tret;
 }
 
 Math.min = function (...values) {
-    return realMin(...values).valueOf();
+    console.debug("Min called with "+values);
+    let tret = realMin(...values).valueOf();
+    console.debug("Returning "+tret);
+    return tret;
 }
 
 Math.max = function (...values) {
-    return realMax(...values).valueOf();
+    console.debug("Max called with "+values);
+    let tret = realMax(...values).valueOf();
+    console.debug("Returning "+tret);
+    return tret;
 }
 
 Math.sin = function (x) {
-    return realSin(x.valueOf()).valueOf();
+    console.debug("Sin called with "+x);
+    let tret = realSin(x.valueOf()).valueOf();;
+    console.debug("Returning "+tret);
+    return tret;
 }
 
 Math.cos = function (x) {
-    return realCos(x.valueOf()).valueOf();
+    console.debug("Cos called with "+x);
+    let tret = realCos(x.valueOf()).valueOf();
+    console.debug("Returning "+tret);
+    return tret;
 }
 
 setTimeout = function(...values) {
     if (values.length > 1){
         values[1] = values[1].valueOf();
     }
+    console.log("SettingTimeout("+values);
     return realTimeout(...values);
 }
 setInterval = function(...values) {
     if (values.length > 1){
         values[1] = values[1].valueOf();
     }
+    console.log("SettingInterval("+values);
     return realInterval(...values);
 }
